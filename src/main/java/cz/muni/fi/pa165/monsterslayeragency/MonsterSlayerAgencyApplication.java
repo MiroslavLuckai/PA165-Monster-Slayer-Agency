2b5cf45fda89057382d358a8eb625656eafa2898
Ludovit Kopcsanyi <kopcsanyiludovit@gmail.com>
2020-03-17 21:06:07 +0100

    Initial commit

---
+++ src/main/java/cz/muni/fi/pa165/monsterslayeragency/MonsterSlayerAgencyApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.monsterslayeragency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterSlayerAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonsterSlayerAgencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/muni/fi/pa165/monsterslayeragency/MonsterSlayerAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.monsterslayeragency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterSlayerAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
