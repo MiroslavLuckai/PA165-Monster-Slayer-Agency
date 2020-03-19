6ba0c6f7e72d6c66fe4581d8a01c3c86ec8f8c1e
Ludovit Kopcsanyi <kopcsanyiludovit@gmail.com>
2020-03-19 12:34:42 +0100

    Introducing Skill enum

---
+++ src/main/java/cz/muni/fi/pa165/monsterslayeragency/enums/Skill.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pa165.monsterslayeragency.enums;
+
+/**
+ * @author Ludovit Kopcsanyi
+ */
+public enum Skill {
+
+	MELEE,
+	BLADES,
+	RANGED,
+	MAGIC,
+	ARMOR,
+	SNEAK,
+	ACROBATICS
+
+}
