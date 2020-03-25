72ff7c0bce43736953e43b832251a1944139c6aa
Filip Daniel Fedin <469210@mail.muni.cz>
2020-03-25 12:43:47 +0100

    Adding hibernate annotations to Monster entity. Adding OTHER value to MonsterType enum.

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Monster.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Monster.java
@@ -1,14 +1,15 @@
 package cz.muni.fi.pa165.monsterslayeragency.entities;
 
 import cz.muni.fi.pa165.monsterslayeragency.enums.MonsterType;
+import cz.muni.fi.pa165.monsterslayeragency.enums.Resistance;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,9 +20,21 @@ import javax.persistence.Id;
 public class Monster {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @Column(nullable = false)
     private int size;
+
+    @Column(nullable = false)
     private MonsterType monsterType;
+
+
+    @ElementCollection
+    private List<Resistance> resitances;
+
     private String image;
 }
