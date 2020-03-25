758bc1a90d22cf20ce67ef29016263ecf6f8ea41
Michaela Bajanová <xbajanov@fi.muni.cz>
2020-03-25 11:54:33 +0100

    using lombok

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Hero.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Hero.java
@@ -1,14 +1,21 @@
 package cz.muni.fi.pa165.monsterslayeragency.entities;
 
-import cz.muni.fi.pa165.monsterslayeragency.enums.Skill;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * @author Ludovit Kopcsanyi
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 @Entity
 public class Hero {
 	@Id
@@ -28,71 +35,4 @@ public class Hero {
 	private Skill[] skills;
 	 */
 	private String image;
-
-	public Hero() {}
-
-	public Hero(User user, String name, Skill[] skills, String image) {
-		this.user = user;
-		this.name = name;
-		//this.skills = skills;
-		this.image = image;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String heroName) {
-		this.name = heroName;
-	}
-/*
-	public Skill[] getSkills() {
-		return skills;
-	}
-
-	public void setSkills(Skill[] skills) {
-		this.skills = skills;
-	}
-*/
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Hero hero = (Hero) o;
-		return getUser().equals(hero.getUser()) &&
-				getName().equals(hero.getName()) &&
-				//Arrays.equals(getSkills(), hero.getSkills()) &&
-				getImage().equals(hero.getImage());
-	}
-
-	@Override
-	public int hashCode() {
-		int result = Objects.hash(getUser(), getName(), getImage());
-		//result = 31 * result + Arrays.hashCode(getSkills());
-		return result;
-	}
 }
