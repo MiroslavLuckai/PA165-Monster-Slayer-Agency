bd32aded92df3a76efc9a88f6771632022712a8b
Michaela Bajanová <xbajanov@fi.muni.cz>
2020-03-25 11:54:43 +0100

    Request entity

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Request.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Request.java
@@ -1,61 +1,47 @@
 package cz.muni.fi.pa165.monsterslayeragency.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * @author Michaela Bajanova (469166)
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
 public class Request {
 
+    @Id
+    @Column(nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    //private Customer customer;
+
+    @Column(nullable = false)
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    private User customer;
+
     private String location;
-    //private Map<EMonsterType, int> monsters;
+
+    // https://stackoverflow.com/questions/21059451/how-to-add-list-as-property-of-entity-in-datastore-of-google-app-engine-basic
+    @OneToMany
+    private List<Monster> monsters;
+
     private BigDecimal award;
-
-    public Request() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public BigDecimal getAward() {
-        return award;
-    }
-
-    public void setAward(BigDecimal award) {
-        this.award = award;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (!(obj instanceof Request))
-            return false;
-        Request other = (Request) obj;
-        return other.getId().equals(this.id);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + id.hashCode();
-        return result;
-    }
 }
