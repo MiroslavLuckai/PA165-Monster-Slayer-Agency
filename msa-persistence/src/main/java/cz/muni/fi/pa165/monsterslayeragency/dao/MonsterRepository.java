71586c8e0d96334f99fe9df3894baaf66021fd2d
Ludovit Kopcsanyi <kopcsanyiludovit@gmail.com>
2020-03-28 13:00:23 +0100

    Reworked MonsterRepository

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/dao/MonsterRepository.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/dao/MonsterRepository.java
@@ -9,7 +9,35 @@ import java.util.List;
 /**
  * @author Ludovit Kopcsanyi
  */
-public interface MonsterRepository extends CrudRepository<Monster, Long> {
+public interface MonsterRepository {
+
+	/**
+	 * Adds a monster to the database
+	 *
+	 * @param monster monster do be added
+	 */
+	public void addMonster(Monster monster);
+
+	/**
+	 * Removes a monster from the database
+	 *
+	 * @param monster monster to be removed
+	 */
+	public void removeMonster(Monster monster);
+
+	/**
+	 * Updates a monster in the database
+	 *
+	 * @param monster monster to be updated
+	 */
+	public void updateMonster(Monster monster);
+
+	/**
+	 * Finds monster by its id
+	 * @param id id of Monster
+	 * @return instance of Monster
+	 */
+	Monster findById(Long id);
 
 	/**
 	 * Finds monster by its name
@@ -21,14 +49,14 @@ public interface MonsterRepository extends CrudRepository<Monster, Long> {
 	/**
 	 * Find monsters by their type
 	 * @param monsterType value of MonsterType
-	 * @return instance of Monster
+	 * @return list of monsters
 	 */
 	List<Monster> findByMonsterType(MonsterType monsterType);
 
 	/**
 	 * Find monsters by their size
 	 * @param size numeric size value
-	 * @return instance of Monster
+	 * @return list of monsters
 	 */
 	List<Monster> findBySize(int size);
 
@@ -36,8 +64,14 @@ public interface MonsterRepository extends CrudRepository<Monster, Long> {
 	 * Find monsters by their type and size
 	 * @param monsterType value of MonsterType
 	 * @param size numeric size value
-	 * @return instance of Monster
+	 * @return list of monsters
 	 */
 	List<Monster> findByMonsterTypeAndSize(MonsterType monsterType, int size);
+
+	/**
+	 * List all monsters
+	 * @return list of monsters
+	 */
+	List<Monster> findAll();
 
 }
