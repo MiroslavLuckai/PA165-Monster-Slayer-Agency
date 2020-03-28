8f935d9a1a7914f6026d988cf05e4ce8ce9c73f1
Filip Daniel Fedin <469210@mail.muni.cz>
2020-03-28 19:36:46 +0100

    Adding hibernate annotations to Monster class.

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Monster.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Monster.java
@@ -1,14 +1,15 @@
 package cz.muni.fi.pa165.monsterslayeragency.entities;
 
 import cz.muni.fi.pa165.monsterslayeragency.enums.MonsterType;
+import cz.muni.fi.pa165.monsterslayeragency.enums.Resistance;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,9 +20,21 @@ import javax.persistence.Id;
 public class Monster {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @Column
     private int size;
+
+    @ElementCollection
+    private List<Resistance> resistances;
+
+    @Column
     private MonsterType monsterType;
+
+    @Column
     private String image;
 }
