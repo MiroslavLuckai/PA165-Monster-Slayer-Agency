31123ba64034ac72a60dd4379d0aa8f765bc743f
Podmenato <49805242+Podmenato@users.noreply.github.com>
2020-03-28 22:35:45 +0100

    Request dao (#10)
    * Introducing interface RequestDao.
    
    * Creating implementation for RequestDao interface.
    
    * Reworking MonsterType enum values.
    
    * Adding hibernate annotations to Monster class.
    
    * Adding author comments.
    
    * Adding throws IllegalArgumentException to method declaration.
    
    * Removing lombok and creating hash code, equals, getters and setters, and constructor.
    
    * Refactoring.
    
    Co-authored-by: Filip Daniel Fedin <469210@mail.muni.cz>

---
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/dao/RequestDaoImpl.java
@@ -0,0 +1,74 @@
+package cz.muni.fi.pa165.monsterslayeragency.dao;
+
+import cz.muni.fi.pa165.monsterslayeragency.entities.Request;
+import cz.muni.fi.pa165.monsterslayeragency.entities.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * @author Filip Daniel Fedin
+ */
+
+@Repository
+@Transactional
+public class RequestDaoImpl implements RequestDao {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void addRequest(Request request) throws IllegalArgumentException{
+        if (request == null) {
+            throw new IllegalArgumentException("Request is null");
+        }
+        em.persist(request);
+    }
+
+    @Override
+    public void removeRequest(Request request) throws IllegalArgumentException{
+        if (request == null) {
+            throw new IllegalArgumentException("Request is null");
+        }
+        em.remove(request);
+    }
+
+    @Override
+    public void updateRequest(Request request) throws IllegalArgumentException{
+        if (request == null) {
+            throw new IllegalArgumentException("Request is null");
+        }
+        em.merge(request);
+    }
+
+    @Override
+    public Request findRequestById(Long id) throws IllegalArgumentException{
+        if (id == null) {
+            throw new IllegalArgumentException("Id is null");
+        }
+        return em.find(Request.class, id);
+    }
+
+    @Override
+    public Request findRequestByCustomer(User user) throws IllegalArgumentException{
+        if (user == null) {
+            throw new IllegalArgumentException("User is null");
+        }
+        try {
+            return em.createQuery("select req from Request req where req.customer = :user", Request.class)
+                    .setParameter("user", user)
+                    .getSingleResult();
+        } catch (NoResultException nrf) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Request> findAll() {
+        return em.createQuery("select req from Request req", Request.class)
+                .getResultList();
+    }
+}
--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Monster.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Monster.java
@@ -1,27 +1,104 @@
 package cz.muni.fi.pa165.monsterslayeragency.entities;
 
 import cz.muni.fi.pa165.monsterslayeragency.enums.MonsterType;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import cz.muni.fi.pa165.monsterslayeragency.enums.Resistance;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
+/**
+ * @author Filip Daniel Fedin
+ */
+
 @Entity
 public class Monster {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @Column
     private int size;
+
+    @ElementCollection
+    private List<Resistance> resistances;
+
+    @Column
     private MonsterType monsterType;
+
+    @Column
     private String image;
+
+    public Monster() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public List<Resistance> getResistances() {
+        return resistances;
+    }
+
+    public MonsterType getMonsterType() {
+        return monsterType;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setResistances(List<Resistance> resistances) {
+        this.resistances = resistances;
+    }
+
+    public void setMonsterType(MonsterType monsterType) {
+        this.monsterType = monsterType;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Monster)) return false;
+        Monster monster = (Monster) o;
+        return getSize() == monster.getSize() &&
+                Objects.equals(getId(), monster.getId()) &&
+                Objects.equals(getName(), monster.getName()) &&
+                Objects.equals(getResistances(), monster.getResistances()) &&
+                getMonsterType() == monster.getMonsterType() &&
+                Objects.equals(getImage(), monster.getImage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getSize(), getResistances(), getMonsterType(), getImage());
+    }
 }
