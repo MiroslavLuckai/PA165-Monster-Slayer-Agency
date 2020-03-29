00c1314c51b7d188d4bccf277285f100a43dc9ca
Podmenato <49805242+Podmenato@users.noreply.github.com>
2020-03-29 22:44:23 +0200

    User tests (#32)
    * Implementing more user tests.
    
    * Fixing hero dao tests.
    
    Co-authored-by: Filip Daniel Fedin <469210@mail.muni.cz>

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/dao/HeroDao.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/dao/HeroDao.java
@@ -10,39 +10,39 @@ public interface HeroDao {
      * Adds hero into database
      * @param hero hero to be added into database
      */
-    void addHero(Hero hero);
+    void addHero(Hero hero) throws IllegalArgumentException;
 
     /**
      * Update hero in database
      * @param hero hero to be updated
      */
-    void updateHero(Hero hero);
+    void updateHero(Hero hero) throws IllegalArgumentException;
 
     /**
      * Remove hero from the database
      * @param hero hero which should be removed
      */
-    void removeHero(Hero hero);
+    void removeHero(Hero hero) throws IllegalArgumentException;
 
     /**
      * find hero by requested id
      * @param id id of the hero we are looking for
-     * @return
+     * @return instance of class Hero with wanted ID
      */
-    Hero findHero(Long id);
+    Hero findHero(Long id) throws IllegalArgumentException;
 
     /**
      * finds hero by its hero nick-name(hero nick-name have to be unique)
      * @param heroName hero nick-name of the user
      * @return instance of class Hero with wanted nick-name, hopefully null otherwise
      */
-    Hero findByHeroName(String heroName);
+    Hero findByHeroName(String heroName) throws IllegalArgumentException;
 
     /**
      * finds hero profile by its underlying user
      * @param userId Id of the user whose hero profile we are looking for
      * @return instance of class Hero with wanted underlying user, hopefully null otherwise
      */
-    Hero findByUser(Long userId);
+    Hero findByUser(Long userId) throws IllegalArgumentException;
 
 }
--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/dao/HeroDaoImpl.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/dao/HeroDaoImpl.java
@@ -16,45 +16,45 @@ public class HeroDaoImpl implements HeroDao {
     private EntityManager em;
 
     @Override
-    public void addHero(Hero hero) {
+    public void addHero(Hero hero) throws IllegalArgumentException {
         validate(hero, "Hero cannot be null!");
         em.persist(hero);
     }
 
     @Override
-    public void updateHero(Hero hero) {
+    public void updateHero(Hero hero) throws IllegalArgumentException {
         validate(hero, "Hero cannot be null!");
         em.merge(hero);
     }
 
     @Override
-    public void removeHero(Hero hero) {
+    public void removeHero(Hero hero) throws IllegalArgumentException {
         validate(hero, "Hero cannot be null!");
         em.remove(hero);
     }
 
     @Override
-    public Hero findHero(Long id) {
+    public Hero findHero(Long id) throws IllegalArgumentException {
         validate(id, "Cannot search for hero with null hero ID!");
         return em.find(Hero.class, id);
     }
 
     @Override
-    public Hero findByHeroName(String heroName) {
+    public Hero findByHeroName(String heroName) throws IllegalArgumentException {
         return em.createQuery("select hero from Hero hero where hero.name = :name", Hero.class)
                 .setParameter("name", heroName)
                 .getSingleResult();
     }
 
     @Override
-    public Hero findByUser(Long userId) {
+    public Hero findByUser(Long userId) throws IllegalArgumentException {
         validate(userId, "Cannot search for hero with null user ID!");
         return em.createQuery("select hero from Hero hero where hero.user.id = :userId", Hero.class)
                 .setParameter("userId", userId)
                 .getSingleResult();
     }
 
-    private void validate(Object object, String message) {
+    private void validate(Object object, String message) throws IllegalArgumentException {
         if (object == null) {
             throw new IllegalArgumentException(message);
         }
