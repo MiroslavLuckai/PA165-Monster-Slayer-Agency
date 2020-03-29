0082e21018220c0d2179bda25af0bd4c0ec63f16
Ludovit Kopcsanyi <kopcsanyiludovit@gmail.com>
2020-03-29 12:03:46 +0200

    Changed User not to use lombok

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/User.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/User.java
@@ -1,21 +1,11 @@
 package cz.muni.fi.pa165.monsterslayeragency.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
+import java.util.Objects;
 
 /**
  * @author Ludovit Kopcsanyi
  */
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
 @Entity
 public class User {
 	@Id
@@ -25,4 +15,71 @@ public class User {
 	private String password;
 	private String userName;
 	private String image;
+
+	public User() {
+	}
+
+	public User(String email, String password, String userName, String image) {
+		this.email = email;
+		this.password = password;
+		this.userName = userName;
+		this.image = image;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return getId() == user.getId() &&
+				Objects.equals(getEmail(), user.getEmail()) &&
+				Objects.equals(getPassword(), user.getPassword()) &&
+				Objects.equals(getUserName(), user.getUserName()) &&
+				Objects.equals(getImage(), user.getImage());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId(), getEmail(), getPassword(), getUserName(), getImage());
+	}
 }
