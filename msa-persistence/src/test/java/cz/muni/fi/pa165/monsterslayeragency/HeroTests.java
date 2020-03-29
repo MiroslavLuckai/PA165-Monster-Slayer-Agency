52811857a4e8b23899a87ee7bd6105b8e8c0883b
Filip Daniel Fedin <469210@mail.muni.cz>
2020-03-29 16:25:13 +0200

    Introducing class for testing Heroes.

---
+++ msa-persistence/src/test/java/cz/muni/fi/pa165/monsterslayeragency/HeroTests.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pa165.monsterslayeragency;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Filip Daniel Fedin
+ */
+
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class HeroTests extends AbstractTestNGSpringContextTests {
+}
