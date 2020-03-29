24891be3e8388597da0fd1a6576a8ebcb10e951b
Filip Daniel Fedin <469210@mail.muni.cz>
2020-03-29 20:21:40 +0200

    Fixing password problem.

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/User.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/User.java
@@ -17,7 +17,7 @@ public class User {
 	@Column(unique = true, nullable = false)
 	private String email;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
 	private String password;
 
     @Column(unique = true, nullable = false)
