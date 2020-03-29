8a053946ecf623e8633199ce730113c54476a963
Krytos <36662583+krytoss@users.noreply.github.com>
2020-03-29 12:35:33 +0200

    Inmemory and context (#18)
    * Introducing InMemoryDatabaseSpring
    
    * Introducing PersistenceSampleApplicationContext

---
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/InMemoryDatabaseSpring.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.pa165.monsterslayeragency;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Ludovit Kopcsanyi
+ */
+@Configuration
+public class InMemoryDatabaseSpring {
+    @Bean
+    public DataSource db() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
+        return db;
+    }
+}
