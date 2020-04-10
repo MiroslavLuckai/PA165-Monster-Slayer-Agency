8933e1394874dda468036ddb78bfeb439e710d01
Filip Daniel Fedin <469210@mail.muni.cz>
2020-04-10 16:40:14 +0200

    Fixing equals and hash code in job. Properly annotating job.

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Hero.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/entities/Hero.java
@@ -12,98 +12,98 @@ import java.util.Set;
 @Entity(name = "Hero")
 @Table(name = "hero")
 public class Hero {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-	@OneToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @OneToOne(targetEntity = User.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-	private User user;
+    private User user;
 
-	@Column(nullable = false, unique = true, name = "name")
-	private String name;
+    @Column(nullable = false, unique = true, name = "name")
+    private String name;
 
-	@Column(name = "skills")
-	@ElementCollection
+    @Column(name = "skills")
+    @ElementCollection
     @Enumerated(EnumType.STRING)
-	private Set<Skill> skills;
+    private Set<Skill> skills;
 
-	@Column(name = "image")
-	private String image;
+    @Column(name = "image")
+    private String image;
 
 
-	public Hero(User user, String name, Set<Skill> skills, String image) {
-		this.user = user;
-		this.name = name;
-		this.skills = skills;
-		this.image = image;
-	}
+    public Hero(User user, String name, Set<Skill> skills, String image) {
+        this.user = user;
+        this.name = name;
+        this.skills = skills;
+        this.image = image;
+    }
 
-	public Hero() {
-	}
+    public Hero() {
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Set<Skill> getSkills() {
-		return skills;
-	}
+    public Set<Skill> getSkills() {
+        return skills;
+    }
 
-	public void setSkills(Set<Skill> skills) {
-		this.skills = skills;
-	}
+    public void setSkills(Set<Skill> skills) {
+        this.skills = skills;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getImage() {
+        return image;
+    }
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+    public void setImage(String image) {
+        this.image = image;
+    }
 
-	public boolean addSkill(Skill skill) {
-	    return this.skills.add(skill);
+    public boolean addSkill(Skill skill) {
+        return this.skills.add(skill);
     }
 
     public boolean removeSkill(Skill skill) {
         return this.skills.remove(skill);
     }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Hero hero = (Hero) o;
-		return getId() == hero.getId() &&
-				Objects.equals(getUser(), hero.getUser()) &&
-				Objects.equals(getName(), hero.getName()) &&
-				Objects.equals(getSkills(), hero.getSkills()) &&
-				Objects.equals(getImage(), hero.getImage());
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return getId() == hero.getId() &&
+                Objects.equals(getUser(), hero.getUser()) &&
+                Objects.equals(getName(), hero.getName()) &&
+                Objects.equals(getSkills(), hero.getSkills()) &&
+                Objects.equals(getImage(), hero.getImage());
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(getId(), getUser(), getName(), getSkills(), getImage());
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUser(), getName(), getSkills(), getImage());
+    }
 }
