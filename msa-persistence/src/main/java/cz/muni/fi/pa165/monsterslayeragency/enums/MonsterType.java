b8329c2684e55ee6454871fb576e3fee4a1bc267
Filip Daniel Fedin <469210@mail.muni.cz>
2020-04-13 20:40:21 +0200

    Adding javadoc to MonsterType

--- msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/enums/MonsterType.java
+++ msa-persistence/src/main/java/cz/muni/fi/pa165/monsterslayeragency/enums/MonsterType.java
@@ -2,8 +2,67 @@ package cz.muni.fi.pa165.monsterslayeragency.enums;
 
 /**
  * @author Michaela Bajanova (469166)
+ *
+ *
+ *
+ * Enum describing to which type the monster belongs
  */
 
 public enum MonsterType {
-    BEAST, CURSED_ONE, DRACONID, ELEMENTA, HYBRID, INSECTOID, NECROPHAGE, OGROID, RELICT, SPECTER, VAMPIRE
+
+    /**
+     * Beasts is the collective term for non-magical creatures
+     */
+    BEAST,
+
+    /**
+     * Beings attributed to this class are usually created as a result of curse
+     */
+    CURSED_ONE,
+
+    /**
+     * Diverse order of quasi-reptiles
+     */
+    DRACONID,
+
+    /**
+     * Entities that are fueled by the elemental power
+     */
+    ELEMENTA,
+
+    /**
+     * Hybrids between two or more species
+     */
+    HYBRID,
+
+    /**
+     * Refers to insect-like monsters
+     */
+    INSECTOID,
+
+    /**
+     *  Refers to monsters that generally haunt cemeteries and battlefields, places where dead bodies lay,
+     *  eating the remains
+     */
+    NECROPHAGE,
+
+    /**
+     * Giant-like creatures
+     */
+    OGROID,
+
+    /**
+     * Ancient creatures
+     */
+    RELICT,
+
+    /**
+     * Spectral creatures that generally haunt cemeteries and crypts
+     */
+    SPECTER,
+
+    /**
+     * Number of creatures that thrive on blood, which contains their victims' life force
+     */
+    VAMPIRE
 }
