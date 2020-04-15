1484acca114d994d75dfae76e0c8996a0bf5b416
Michaela Bajanová <xbajanov@fi.muni.cz>
2020-04-15 16:07:29 +0200

    JobServiceImpl skeleton

---
+++ msa-service/src/main/java/cz/muni/fi/pa165/msa/service/JobServiceImpl.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.pa165.msa.service;
+
+import cz.muni.fi.pa165.monsterslayeragency.entities.Job;
+
+import java.util.List;
+
+/**
+ * @author Michaela Bajanova (469166)
+ */
+public class JobServiceImpl implements JobService {
+
+    @Override
+    public Job createJob(Job job) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public void updateJob(Job job) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void deleteJob(Job job) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public Job findById(Long id) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public List<Job> findAll() {
+        return null;
+    }
+}
