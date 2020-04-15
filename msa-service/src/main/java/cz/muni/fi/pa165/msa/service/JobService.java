2575f3c0ed458bda9c35ac58001c1540299a0506
Michaela Bajanová <xbajanov@fi.muni.cz>
2020-04-15 16:38:18 +0200

    added service annotation

--- msa-service/src/main/java/cz/muni/fi/pa165/msa/service/JobService.java
+++ msa-service/src/main/java/cz/muni/fi/pa165/msa/service/JobService.java
@@ -1,12 +1,14 @@
 package cz.muni.fi.pa165.msa.service;
 
 import cz.muni.fi.pa165.monsterslayeragency.entities.Job;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * @author Michaela Bajanova (469166)
  */
+@Service
 public interface JobService {
 
     Job createJob(Job job) throws IllegalArgumentException;
