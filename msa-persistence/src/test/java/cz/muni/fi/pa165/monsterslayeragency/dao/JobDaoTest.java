0ceffad6e37178003ea87e84be1f645890025c40
Ludovit Kopcsanyi <kopcsanyiludovit@gmail.com>
2020-04-16 03:59:00 +0200

    Changed the way setUp() method works in tests, added Monster, Request, User and Hero to JobDaotest setUp

--- msa-persistence/src/test/java/cz/muni/fi/pa165/monsterslayeragency/dao/JobDaoTest.java
+++ msa-persistence/src/test/java/cz/muni/fi/pa165/monsterslayeragency/dao/JobDaoTest.java
@@ -1,8 +1,10 @@
 package cz.muni.fi.pa165.monsterslayeragency.dao;
 
 import cz.muni.fi.pa165.monsterslayeragency.PersistenceSampleApplicationContext;
-import cz.muni.fi.pa165.monsterslayeragency.entities.Job;
+import cz.muni.fi.pa165.monsterslayeragency.entities.*;
+import cz.muni.fi.pa165.monsterslayeragency.enums.JobSeverity;
 import cz.muni.fi.pa165.monsterslayeragency.enums.JobStatus;
+import cz.muni.fi.pa165.monsterslayeragency.enums.MonsterType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -15,7 +17,12 @@ import org.testng.annotations.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Ludovit Kopcsanyi
@@ -32,29 +39,75 @@ public class JobDaoTest extends AbstractTestNGSpringContextTests {
     JobDao jobDao;
 
     private Job job;
+    private Request request;
+    private Monster monster;
+    private User user;
+    private Hero hero;
 
     @BeforeMethod
     public void setUp() {
+        monster = new Monster();
+        monster.setName("Ghoul");
+        monster.setSize(10);
+        monster.setMonsterType(MonsterType.NECROPHAGE);
+
+        request = new Request();
+        request.setLocation("Velen");
+        request.setAward(new BigDecimal(100));
+        request.setMonsters(new ArrayList<>());
+        request.getMonsters().add(monster);
+
+        user = new User();
+        user.setEmail("default@mail.com");
+        user.setImage("test_image");
+        user.setPassword("default");
+        user.setUserName("Geralt");
+
+        hero = new Hero();
+        hero.setName("Witcher");
+        hero.setUser(user);
+        hero.setImage("test");
+
+        entityManager.persist(user);
+        entityManager.persist(hero);
+        entityManager.persist(monster);
+        entityManager.persist(request);
+
         job = new Job();
+        job.setHeroes(new HashSet<>());
+        job.getHeroes().add(hero);
+        job.setEvaluation(5);
+        job.setRequest(request);
+        job.setSeverity(JobSeverity.MODERATE);
+        job.setStatus(JobStatus.DONE);
+    }
+
+    @Test
+    public void addJobTest() {
         jobDao.addJob(job);
+
+        List<Job> resultList = entityManager.createQuery("select j from Job j", Job.class).getResultList();
+        assertThat(resultList).containsExactlyInAnyOrder(job);
     }
 
     @Test
     public void testFindJobById() {
+        entityManager.persist(job);
         Job found = jobDao.findJobById(job.getId());
-        Assert.assertEquals(job.getId(), found.getId());
+        Assert.assertEquals(job, found);
     }
 
     @Test
     public void testFindAll() {
+        entityManager.persist(job);
         Job job2 = new Job();
-        jobDao.addJob(job2);
+        entityManager.persist(job2);
         List<Job> found = jobDao.findAll();
-        Assert.assertEquals(found.size(), 2);
-    }
+        assertThat(found).containsExactlyInAnyOrder(job, job2);    }
 
     @Test
     public void testUpdate() {
+        entityManager.persist(job);
         Job found = jobDao.findJobById(job.getId());
         found.setStatus(JobStatus.DONE);
         jobDao.updateJob(found);
@@ -64,6 +117,7 @@ public class JobDaoTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testRemove() {
+        entityManager.persist(job);
         jobDao.removeJob(job);
         Assert.assertNull(jobDao.findJobById(job.getId()));
     }
--- msa-persistence/src/test/java/cz/muni/fi/pa165/monsterslayeragency/dao/UserDaoTests.java
+++ msa-persistence/src/test/java/cz/muni/fi/pa165/monsterslayeragency/dao/UserDaoTests.java
@@ -18,6 +18,8 @@ import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author Filip Daniel Fedin
  */
@@ -43,25 +45,57 @@ public class UserDaoTests extends AbstractTestNGSpringContextTests{
         user1.setImage("test_image");
         user1.setPassword("default");
         user1.setUserName("Geralt");
-        userDao.addUser(user1);
 
         user2 = new User();
         user2.setEmail("normal@mail.com");
         user2.setImage("image");
         user2.setUserName("Peter");
         user2.setPassword("password");
+    }
+
+    @Test
+    public void addUserTest() {
+        userDao.addUser(user1);
         userDao.addUser(user2);
+
+        List<User> resultList = em.createQuery("select u from User u", User.class).getResultList();
+        assertThat(resultList).containsExactlyInAnyOrder(user1, user2);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void addUserWithNoUserNameTest() {
+        user1.setUserName(null);
+        userDao.addUser(user1);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void addUserWithNoPasswordTest() {
+        user1.setPassword(null);
+        userDao.addUser(user1);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void addUserWithNoEmailTest() {
+        user1.setEmail(null);
+        userDao.addUser(user1);
     }
 
 
     @Test
     public void findUserByIdTest() {
+        em.persist(user1);
         User found = userDao.findUserById(user1.getId());
         Assert.assertEquals(user1, found);
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void findUserByNullIdTest() {
+        userDao.findUserById(null);
+    }
+
     @Test
     public void updateUserTest() {
+        em.persist(user1);
         String oldMail = user1.getEmail();
         user1.setEmail("newMail");
         userDao.updateUser(user1);
@@ -70,26 +104,53 @@ public class UserDaoTests extends AbstractTestNGSpringContextTests{
         Assert.assertNotEquals(found.getEmail(), oldMail);
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void updateNullUserTest() {
+        userDao.updateUser(null);
+    }
+
     @Test
     public void removeUserTest() {
+        em.persist(user1);
         userDao.removeUser(user1);
         Assert.assertNull(userDao.findUserById(user1.getId()));
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void removeNullUserTest() {
+        userDao.removeUser(null);
+    }
+
     @Test
     public void findUserByEmailTest() {
+        em.persist(user1);
         User found = userDao.findUserByEmail(user1.getEmail());
         Assert.assertEquals(found, user1);
     }
 
     @Test
+    public void findUserByNotExistingEmailTest() {
+        User found = userDao.findUserByEmail("blabla");
+        Assert.assertNull(found);
+    }
+
+    @Test
     public void findUserByUsernameTest() {
+        em.persist(user1);
         User found = userDao.findUserByUsername(user1.getUserName());
         Assert.assertEquals(found, user1);
     }
 
     @Test
+    public void findUserByNotExistingUsernameTest() {
+        User found = userDao.findUserByUsername("blabal");
+        Assert.assertNull(found);
+    }
+
+    @Test
     public void findAllUsersTest() {
+        em.persist(user1);
+        em.persist(user2);
         List<User> found = userDao.getAllUsers();
         List<User> users = new ArrayList<>();
         users.add(user1);
