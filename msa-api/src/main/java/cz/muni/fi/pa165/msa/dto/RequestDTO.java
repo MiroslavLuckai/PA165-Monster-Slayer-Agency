c6e7e81a43ae5dd9cd4090ad6277fc073a6110d0
Filip Daniel Fedin <469210@mail.muni.cz>
2020-04-19 21:53:54 +0200

    Creating RequestDTO.

---
+++ msa-api/src/main/java/cz/muni/fi/pa165/msa/dto/RequestDTO.java
@@ -0,0 +1,90 @@
+package cz.muni.fi.pa165.msa.dto;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author Filip Daniel Fedin
+ */
+public class RequestDTO {
+
+    private Long id;
+
+//    private UserDTO customer;
+
+    private String location;
+
+//    private Set<MonsterDTO> heroes;
+
+    private BigDecimal award;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+//    public UserDTO getCustomer() {
+//        return customer;
+//    }
+//
+//    public void setCustomer(UserDTO customer) {
+//        this.customer = customer;
+//    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+//    public Set<MonsterDTO> getHeroes() {
+//        return heroes;
+//    }
+//
+//    public void setHeroes(Set<MonsterDTO> heroes) {
+//        this.heroes = heroes;
+//    }
+
+    public BigDecimal getAward() {
+        return award;
+    }
+
+    public void setAward(BigDecimal award) {
+        this.award = award;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof RequestDTO)) return false;
+//        RequestDTO that = (RequestDTO) o;
+//        return Objects.equals(getId(), that.getId()) &&
+//                Objects.equals(getCustomer(), that.getCustomer()) &&
+//                Objects.equals(getLocation(), that.getLocation()) &&
+//                Objects.equals(getHeroes(), that.getHeroes()) &&
+//                Objects.equals(getAward(), that.getAward());
+//    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId(), getCustomer(), getLocation(), getHeroes(), getAward());
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "RequestDTO{" +
+//                "id=" + id +
+//                ", customer=" + customer +
+//                ", location='" + location + '\'' +
+//                ", heroes=" + heroes +
+//                ", award=" + award +
+//                '}';
+//    }
+
+}
