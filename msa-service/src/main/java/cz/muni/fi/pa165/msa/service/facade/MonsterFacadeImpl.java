84bfe32b5a2c1d11c599f211cc7d350d3f840f64
Ludovit Kopcsanyi <kopcsanyiludovit@gmail.com>
2020-04-20 21:48:42 +0200

    Created MonsterFacadeImpl

--- msa-service/src/main/java/cz/muni/fi/pa165/msa/service/facade/MonsterFacadeImpl.java
+++ msa-service/src/main/java/cz/muni/fi/pa165/msa/service/facade/MonsterFacadeImpl.java
@@ -1,4 +1,47 @@
 package cz.muni.fi.pa165.msa.service.facade;
 
-public class MonsterFacadeImpl {
+import cz.muni.fi.pa165.monsterslayeragency.enums.MonsterType;
+import cz.muni.fi.pa165.msa.dto.MonsterCreateDTO;
+import cz.muni.fi.pa165.msa.dto.MonsterDTO;
+import cz.muni.fi.pa165.msa.facade.MonsterFacade;
+
+import java.util.List;
+
+public class MonsterFacadeImpl implements MonsterFacade {
+
+    @Override
+    public Long createMonster(MonsterCreateDTO monster) {
+        return null;
+    }
+
+    @Override
+    public boolean removeMonster(Long id) {
+        return false;
+    }
+
+    @Override
+    public MonsterDTO findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<MonsterDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public MonsterDTO findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<MonsterDTO> findByMonsterType(MonsterType monsterType) {
+        return null;
+    }
+
+    @Override
+    public List<MonsterDTO> findBySize(int size) {
+        return null;
+    }
+
 }
