7da70dec7c6306c62bc4704675987387aae5a96e
Podmenato <49805242+Podmenato@users.noreply.github.com>
2020-04-21 11:42:37 +0200

    Creating mapping service and configuration (#44)
    Co-authored-by: Filip Daniel Fedin <469210@mail.muni.cz>

---
+++ msa-service/src/main/java/cz/muni/fi/pa165/msa/service/BeanMappingService.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.msa.service;
+
+import org.dozer.Mapper;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface BeanMappingService {
+    <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
+
+    <T> T mapTo(Object u, Class<T> mapToClass);
+    Mapper getMapper();
+}
