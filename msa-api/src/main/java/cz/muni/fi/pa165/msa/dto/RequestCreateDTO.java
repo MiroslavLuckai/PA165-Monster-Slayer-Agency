07a759a02449a765957423c02e40847458369eef
Michaela Bajanová <xbajanov@fi.muni.cz>
2020-04-24 14:49:18 +0200

    uncommenting dto attributes

--- msa-api/src/main/java/cz/muni/fi/pa165/msa/dto/RequestCreateDTO.java
+++ msa-api/src/main/java/cz/muni/fi/pa165/msa/dto/RequestCreateDTO.java
@@ -6,21 +6,18 @@ import java.util.Set;
 
 public class RequestCreateDTO {
 
-//    private UserDTO customer;
-
+    private UserDTO customer;
     private String location;
-
-//    private Set<MonsterDTO> heroes;
-
+    private Set<MonsterDTO> heroes;
     private BigDecimal award;
 
-//    public UserDTO getCustomer() {
-//        return customer;
-//    }
-//
-//    public void setCustomer(UserDTO customer) {
-//        this.customer = customer;
-//    }
+    public UserDTO getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(UserDTO customer) {
+        this.customer = customer;
+    }
 
     public String getLocation() {
         return location;
@@ -30,13 +27,13 @@ public class RequestCreateDTO {
         this.location = location;
     }
 
-//    public Set<MonsterDTO> getHeroes() {
-//        return heroes;
-//    }
-//
-//    public void setHeroes(Set<MonsterDTO> heroes) {
-//        this.heroes = heroes;
-//    }
+    public Set<MonsterDTO> getHeroes() {
+        return heroes;
+    }
+
+    public void setHeroes(Set<MonsterDTO> heroes) {
+        this.heroes = heroes;
+    }
 
     public BigDecimal getAward() {
         return award;
@@ -46,29 +43,29 @@ public class RequestCreateDTO {
         this.award = award;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (!(o instanceof RequestCreateDTO)) return false;
-//        RequestCreateDTO that = (RequestCreateDTO) o;
-//        return Objects.equals(getCustomer(), that.getCustomer()) &&
-//                Objects.equals(getLocation(), that.getLocation()) &&
-//                Objects.equals(getHeroes(), that.getHeroes()) &&
-//                Objects.equals(getAward(), that.getAward());
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(getCustomer(), getLocation(), getHeroes(), getAward());
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "RequestCreateDTO{" +
-//                "customer=" + customer +
-//                ", location='" + location + '\'' +
-//                ", heroes=" + heroes +
-//                ", award=" + award +
-//                '}';
-//    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RequestCreateDTO)) return false;
+        RequestCreateDTO that = (RequestCreateDTO) o;
+        return Objects.equals(getCustomer(), that.getCustomer()) &&
+                Objects.equals(getLocation(), that.getLocation()) &&
+                Objects.equals(getHeroes(), that.getHeroes()) &&
+                Objects.equals(getAward(), that.getAward());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCustomer(), getLocation(), getHeroes(), getAward());
+    }
+
+    @Override
+    public String toString() {
+        return "RequestCreateDTO{" +
+                "customer=" + customer +
+                ", location='" + location + '\'' +
+                ", heroes=" + heroes +
+                ", award=" + award +
+                '}';
+    }
 }
