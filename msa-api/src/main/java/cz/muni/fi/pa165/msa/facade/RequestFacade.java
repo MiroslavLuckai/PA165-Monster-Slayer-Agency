f8c0ea13e9c0d619f0c0b49d6eb33bd1004eb2af
Filip Daniel Fedin <469210@mail.muni.cz>
2020-04-26 16:31:55 +0200

    Adding management methods to facade

--- msa-api/src/main/java/cz/muni/fi/pa165/msa/facade/RequestFacade.java
+++ msa-api/src/main/java/cz/muni/fi/pa165/msa/facade/RequestFacade.java
@@ -1,9 +1,12 @@
 package cz.muni.fi.pa165.msa.facade;
 
+import cz.muni.fi.pa165.monsterslayeragency.entities.Monster;
+import cz.muni.fi.pa165.monsterslayeragency.entities.Request;
 import cz.muni.fi.pa165.msa.dto.RequestCreateDTO;
 import cz.muni.fi.pa165.msa.dto.RequestDTO;
 import cz.muni.fi.pa165.msa.dto.UserDTO;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface RequestFacade {
@@ -40,5 +43,33 @@ public interface RequestFacade {
      * @return request with the selected customer
      */
     RequestDTO findByCustomer(UserDTO customer);
+
+    /**
+     * Adds a monster to the request
+     * @param request request
+     * @param monster monster to be added
+     */
+    void addMonster(Request request, Monster monster);
+
+    /**
+     * Removes a monster from the request
+     * @param request request
+     * @param monster monster to be removed
+     */
+    void removeMonster(Request request, Monster monster);
+
+    /**
+     * Changes the location of the request
+     * @param request request
+     * @param location location to be changed
+     */
+    void changeLocation(Request request, String location);
+
+    /**
+     * Changes the reward from the request
+     * @param request request
+     * @param award number of money awarded for completion
+     */
+    void changeAward(Request request, BigDecimal award);
 
 }
