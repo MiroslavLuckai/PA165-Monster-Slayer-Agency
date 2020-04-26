2ebd7ec22405c6cafde7f4dfd0e7452f71bf5051
Ludovit Kopcsanyi <kopcsanyiludovit@gmail.com>
2020-04-26 17:03:41 +0200

    Added size to MonsterCreateDTO

--- msa-api/src/main/java/cz/muni/fi/pa165/msa/dto/MonsterCreateDTO.java
+++ msa-api/src/main/java/cz/muni/fi/pa165/msa/dto/MonsterCreateDTO.java
@@ -18,6 +18,15 @@ public class MonsterCreateDTO {
 	private MonsterType monsterType;
 	private String image;
 	private Food food;
+	private int size;
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
 
 	public String getName() {
 		return name;
