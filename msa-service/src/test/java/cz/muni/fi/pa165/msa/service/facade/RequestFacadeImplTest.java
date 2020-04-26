1de7a613de7d2b1745debaa53e4fc1dc680af737
Filip Daniel Fedin <469210@mail.muni.cz>
2020-04-26 21:24:54 +0200

    Adding test for mapping from create dto

--- msa-service/src/test/java/cz/muni/fi/pa165/msa/service/facade/RequestFacadeImplTest.java
+++ msa-service/src/test/java/cz/muni/fi/pa165/msa/service/facade/RequestFacadeImplTest.java
@@ -1,103 +1,138 @@
 package cz.muni.fi.pa165.msa.service.facade;
 
-import cz.muni.fi.pa165.monsterslayeragency.dao.MonsterDao;
-import cz.muni.fi.pa165.monsterslayeragency.dao.RequestDao;
-import cz.muni.fi.pa165.monsterslayeragency.dao.UserDao;
 import cz.muni.fi.pa165.monsterslayeragency.entities.Monster;
 import cz.muni.fi.pa165.monsterslayeragency.entities.Request;
-import cz.muni.fi.pa165.msa.dto.RequestCreateDTO;
-import cz.muni.fi.pa165.msa.dto.RequestDTO;
-import cz.muni.fi.pa165.msa.facade.RequestFacade;
-import cz.muni.fi.pa165.msa.service.*;
-import cz.muni.fi.pa165.msa.service.config.ServiceConfiguration;
-import org.hibernate.service.spi.ServiceException;
+import cz.muni.fi.pa165.monsterslayeragency.entities.User;
+import cz.muni.fi.pa165.monsterslayeragency.enums.MonsterType;
+import cz.muni.fi.pa165.monsterslayeragency.enums.Resistance;
+import cz.muni.fi.pa165.msa.dto.*;
+import cz.muni.fi.pa165.msa.service.BeanMappingService;
+import cz.muni.fi.pa165.msa.service.DummyObjects;
+import cz.muni.fi.pa165.msa.service.MonsterServiceImpl;
+import cz.muni.fi.pa165.msa.service.RequestServiceImpl;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
-@ContextConfiguration(classes = ServiceConfiguration.class)
-public class RequestFacadeImplTest extends AbstractTestNGSpringContextTests {
+/**
+ * @author Ludovit Kopcsanyi
+ */
+public class RequestFacadeImplTest {
 
-
-    @Mock
-    private RequestService requestService;
+    @Autowired
+    @InjectMocks
+    private RequestFacadeImpl requestFacade;
 
     @Mock
     private BeanMappingService beanMappingService;
 
+    @Mock
+    private RequestServiceImpl requestService;
 
+    private Request request1;
+    private Request request2;
 
-    @Autowired
-    @InjectMocks
-    private RequestFacade requestFacade;
+    private User customer;
 
-    private RequestDTO requestDTO;
+    private RequestDTO requestDTO1;
+    private RequestDTO requestDTO2;
+
+    private UserDTO userDTO;
+
     private RequestCreateDTO requestCreateDTO;
-    private Request request;
 
     @BeforeClass
-    public void init() throws ServiceException {
+    void initMocks() {
         MockitoAnnotations.initMocks(this);
     }
 
     @BeforeMethod
-    public void setUp() {
+    void setUp() {
+        customer = DummyObjects.getUserDummy1();
+        userDTO = DummyObjects.getUserDTODummy2();
 
-        request = DummyObjects.getRequestDummy1();
-
-        requestDTO = DummyObjects.getRequestDTODummy1();
+        requestDTO1 = DummyObjects.getRequestDTODummy1();
+        requestDTO2 = DummyObjects.getRequestDTODummy2();
 
         requestCreateDTO = new RequestCreateDTO();
-        requestCreateDTO.setAward(requestDTO.getAward());
-        requestCreateDTO.setCustomer(requestDTO.getCustomer());
-        requestCreateDTO.setLocation(requestDTO.getLocation());
-        requestCreateDTO.setMonsters(requestDTO.getMonsters());
+        requestCreateDTO.setCustomer(userDTO);
+        requestCreateDTO.setAward(requestDTO1.getAward());
+        requestCreateDTO.setMonsters(requestDTO1.getMonsters());
+        requestCreateDTO.setLocation(requestDTO1.getLocation());
 
 
-        when(beanMappingService.mapTo(requestDTO, Request.class)).thenReturn(request);
-        when(beanMappingService.mapTo(requestCreateDTO, Request.class)).thenReturn(request);
-        when(beanMappingService.mapTo(request, RequestDTO.class)).thenReturn(requestDTO);
 
-        when(requestService.findById(request.getId())).thenReturn(request);
-        when(requestService.findByCustomer(request.getCustomer())).thenReturn(request);
-        when(requestService.create(request)).thenReturn(request);
+        request1 = DummyObjects.getRequestDummy1();
+        request2 = DummyObjects.getRequestDummy2();
     }
 
     @AfterMethod
-    private void resetMocks() {
+    void reset() {
         Mockito.reset(requestService);
     }
 
-
     @Test
-    public void testCreateRequest() {
-
+    void create() {
+        when(beanMappingService.mapTo(requestCreateDTO, Request.class)).thenReturn(request1);
+        when(requestService.create(request1)).thenReturn(request1);
+        Long createdId = requestFacade.createRequest(requestCreateDTO);
+        verify(requestService).create(request1);
+        Assert.assertEquals(createdId, request1.getId());
     }
 
     @Test
-    public void testRemoveRequest() {
+    void delete() {
+        doNothing().when(requestService).delete(any());
+        when(requestService.findById(request1.getId())).thenReturn(request1);
+        requestFacade.removeRequest(request1.getId());
+        verify(requestService).delete(request1);
     }
 
     @Test
-    public void testFindById() {
+    void findRequestById() {
+        when(beanMappingService.mapTo(request1, RequestDTO.class)).thenReturn(requestDTO1);
+        when(requestService.findById(request1.getId())).thenReturn(request1);
+        RequestDTO tempRequestDTO = requestFacade.findById(request1.getId());
+        Assert.assertEquals(tempRequestDTO, requestDTO1);
     }
 
     @Test
-    public void testFindAll() {
+    void findRequestByCustomer() {
+        when(beanMappingService.mapTo(request1, RequestDTO.class)).thenReturn(requestDTO1);
+        when(requestService.findByCustomer(request1.getCustomer())).thenReturn(request1);
+        RequestDTO tempRequestDTO = requestFacade.findByCustomer(requestDTO1.getCustomer());
+        Assert.assertEquals(tempRequestDTO, requestDTO1);
     }
 
     @Test
-    public void testFindByCustomer() {
+    void findAllRequests() {
+        List<Request> requestList = new ArrayList<>();
+        requestList.add(request1);
+        requestList.add(request2);
+        when(requestService.findAll()).thenReturn(requestList);
+
+        List<RequestDTO> requestDTOs = new ArrayList<>();
+        requestDTOs.add(requestDTO1);
+        requestDTOs.add(requestDTO2);
+        when(beanMappingService.mapTo(requestDTOs, RequestDTO.class)).thenReturn(requestDTOs);
+
+        List<RequestDTO> actualRequestDTO = requestFacade.findAll();
+        verify(requestService).findAll();
+        Assert.assertEquals(actualRequestDTO, requestDTOs);
     }
+
 }
