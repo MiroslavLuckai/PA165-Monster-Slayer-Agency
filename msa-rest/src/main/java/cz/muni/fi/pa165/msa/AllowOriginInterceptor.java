8e73d9a8ca9d86d6c6132bc0312137f90ab7ea2b
Michaela Bajanová <xbajanov@fi.muni.cz>
2020-05-16 21:25:12 +0200

    adding header

--- msa-rest/src/main/java/cz/muni/fi/pa165/msa/AllowOriginInterceptor.java
+++ msa-rest/src/main/java/cz/muni/fi/pa165/msa/AllowOriginInterceptor.java
@@ -14,6 +14,7 @@ public class AllowOriginInterceptor extends HandlerInterceptorAdapter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods",
                 "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         return true;
     }
 
