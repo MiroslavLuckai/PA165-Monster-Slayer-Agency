fd274611b8a211bed2105d0e56da333a059312f6
Miroslav Lučkai <miroslav.luckai@gmail.com>
2020-05-31 15:19:43 +0200

    Swagger documentation is working now

--- msa-rest/src/main/java/cz/muni/fi/pa165/msa/RootWebContext.java
+++ msa-rest/src/main/java/cz/muni/fi/pa165/msa/RootWebContext.java
@@ -13,10 +13,14 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -26,10 +30,11 @@ import java.util.Locale;
  * @author Ludovit Kopcsanyi
  */
 @EnableWebMvc
+@EnableSwagger2
 @Configuration
 @Import({ServiceConfiguration.class, MSAWithSampleDataConfiguration.class})
 @ComponentScan(basePackages = {"cz.muni.fi.pa165.msa.rest.controllers"})
-public class RootWebContext implements WebMvcConfigurer {
+public class RootWebContext extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -50,18 +55,43 @@ public class RootWebContext implements WebMvcConfigurer {
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH));
 
-//        objectMapper.addMixIn(ProductDTO.class, ProductDTOMixin.class);
-//        objectMapper.addMixIn(UserDTO.class, UserDTOMixin.class);
-
         objectMapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
 
         jsonConverter.setObjectMapper(objectMapper);
         return jsonConverter;
     }
-
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(customJackson2HttpMessageConverter());
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Monster Slayer Agency")
+                .description("\"Monster slayer agency application.\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
